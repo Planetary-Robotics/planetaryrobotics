9fc6970c9a55430eda23a47b37289445081e18c8
taylorpechacek-atlassian <53586336+taylorpechacek-atlassian@users.noreply.github.com>
2020-07-22 16:35:21 +1000

    PROBO-11-update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Open Toolchain.
+          New toolchain
         </a>
       </header>
     </div>
