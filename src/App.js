fbe67874d20c2daa0e9be55ecc2c49edd534d183
Taylor Pechacek <tpechacek@atlassian.com>
2020-10-29 15:03:58 +1100

    PROBO-29 added changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Jenkins is now working! What about now?
+          Jenkins is now working! What about now? And now?
         </a>
       </header>
     </div>
